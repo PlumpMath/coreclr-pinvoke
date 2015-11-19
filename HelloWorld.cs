e1244c81eb7f3699613a0f70b2ffaab57930bf0c
Arthur Pham <arthur.pham@gmail.com>
2015-11-19 20:43:30 +0000

    Working sample of a coreclr sample under linux calling a c function

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,7 +1,15 @@
 using System;
+using System.Runtime.InteropServices;
 
 public class Program {
+    
+    [DllImport("ffi_test.so")]
+    private static extern int ffi_test();
+    
 	public static void Main(string[] args){
 		Console.WriteLine("Hello World from Core CLR!");
+		
+		var answer = ffi_test();
+        System.Console.Out.WriteLine("The answer is of course: " + answer);
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coreclr-pinvoke
+
+Under linux follow https://github.com/dotnet/coreclr/blob/master/Documentation/install/get-dotnetcore-dnx-linux.md
+To compile the c file : gcc -shared -o ffi_test.so ffi_test.c
+LD_LIBRARY_PATH=.  dnx run
